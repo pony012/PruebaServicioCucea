d33d3af3c7a0227f3d998a665b9e2204cbef2ff2
Alan <pony_alanandres@hotmail.com>
2016-10-27 12:27:39 -0500

    Se deja preparado para hacer deploy a bluemix.

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
+import cf_deployment_tracker
+import os
 from app.app import app
 
+cf_deployment_tracker.track()
+
+# On Bluemix, get the port number from the environment variable VCAP_APP_PORT
+# When running this app on the local machine, default the port to 8080
+port = int(os.getenv('VCAP_APP_PORT', 8080))
+
+host = str(os.getenv('VCAP_APP_HOST', "127.0.0.1"))
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host = host, port = port)
