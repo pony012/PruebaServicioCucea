38da801ab038a97f4fa160f36f4221f465750334
Alan Sanchez <pony_alanandres@hotmail.com>
2016-11-12 00:43:50 -0600

    Se crea la tabla registro como prueba, después se moverá a models.

--- app/app.py
+++ app/app.py
@@ -8,6 +8,18 @@ from models.User import User
 # from models.Role import Role
 
 app = config.app
+db_sql = config.db_sql
+
+
+class Registro(db_sql.Model):
+    id = db_sql.Column(db_sql.Integer, primary_key=True)
+    user = db_sql.Column(db_sql.Integer, unique=False)
+
+    def __init__(self, user):
+        self.user = user
+
+    def __repr__(self):
+        return '<Registro %r | %r>' % self.id, self.user
 
 
 # Create a user to test with
