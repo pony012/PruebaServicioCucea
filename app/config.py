433c2823fa65c51b8f906105feec01819a17c176
Alan Sanchez <pony_alanandres@hotmail.com>
2016-11-12 00:43:20 -0600

    Configuración de base de datos SQL

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_mongoengine import MongoEngine
 from flask_security import Security, MongoEngineUserDatastore, \
      login_required
+from flask_sqlalchemy import SQLAlchemy
 import json
 #
 # Create app
@@ -11,7 +12,8 @@ app.config['SECRET_KEY'] = '123456789'
 
 app.config['PORT'] = int(os.getenv('VCAP_APP_PORT', 5000))
 
-app.config['HOST'] = '127.0.0.1' if "VCAP_APP_HOST" in os.environ else '0.0.0.0'
+app.config['HOST'] = '127.0.0.1' if "VCAP_APP_HOST" in os.environ \
+                                else '0.0.0.0'
 
 app.config['DEBUG'] = True if "VCAP_APP_HOST" in os.environ else True
 
@@ -25,12 +27,17 @@ if 'VCAP_SERVICES' in os.environ:
     app.config['MONGODB_PORT'] = int(mongodbCred['port'])
     app.config['MONGODB_USERNAME'] = str(mongodbCred['username'])
     app.config['MONGODB_PASSWORD'] = str(mongodbCred['password'])
+    # TODO: Load mysql config from VCAP_SERVICES
 else:
     app.config['MONGODB_DB'] = 'prueba'
     app.config['MONGODB_HOST'] = 'localhost'
     app.config['MONGODB_PORT'] = 27017
+    mysql_uri = 'mysql://alan:testpassword@localhost/PruebaFlask'
+    app.config['SQLALCHEMY_DATABASE_URI'] = mysql_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 #
 # app.config['SECURITY_TRACKABLE'] = True
 #
 # Create database connection object
 db = MongoEngine(app)
+db_sql = SQLAlchemy(app)
